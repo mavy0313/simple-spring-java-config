d5b6183a18fb10d9b1a3a9bd47c838ef8fbe2fc0
mavy0313 <vypoff@gmail.com>
2018-11-25 11:08:15 +0400

    ApplicationContext, @ComponentScan, @Configuration

--- src/main/java/spring/java/config/JavaConfig.java
+++ src/main/java/spring/java/config/JavaConfig.java
@@ -1,13 +1,17 @@
 package spring.java.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import spring.java.service.BlogPostService;
 import spring.java.service.impl.BlogPostServiceImpl;
 
+//@Configuration
+@ComponentScan(basePackages= {"spring.java"})
 public class JavaConfig {
 
-	@Bean //default bean name is methodName
+	@Bean //default bean id is methodName
 	public BlogPostService blogPostService() {
 		return new BlogPostServiceImpl();
 	}
--- src/main/java/spring/java/config/MainApplication.java
+++ src/main/java/spring/java/config/MainApplication.java
@@ -1,12 +1,38 @@
 package spring.java.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import spring.java.domain.BlogPost;
+import spring.java.domain.DataSource;
+import spring.java.service.BlogPostService;
 
 public class MainApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(MainApplication.class);
+	
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(JavaConfig.class);
+		
+//		BlogPostService blogPostService = applicationContext.getBean(BlogPostService.class);
+		
+		BlogPostService blogPostService = (BlogPostService) applicationContext.getBean("blogPostService");
+		
+		BlogPost blogPost = new BlogPost();
+		blogPost.setId(1L);
+		blogPost.setTitle("Java Spring MVC Java Config");
+		
+		blogPostService.savePost(blogPost);
+		
+		DataSource dataSource = applicationContext.getBean(DataSource.class);
+		LOGGER.debug("dataSource: " + dataSource.getUrl());
+		LOGGER.debug("dataSource: " + dataSource.getUsername());
+		LOGGER.debug("dataSource: " + dataSource.getPassword());
+		
+		((ConfigurableApplicationContext) applicationContext).close(); 
 	}
 
 }
